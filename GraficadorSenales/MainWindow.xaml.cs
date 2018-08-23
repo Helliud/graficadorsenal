ddcd0025f935ba139834c4ba95272139c8e094f7
Javier Eliud <42249118+Helliud@users.noreply.github.com>
2018-08-23 10:45:35 -0700

    Ya porfin esta terminado

--- GraficadorSenales/MainWindow.xaml.cs
+++ GraficadorSenales/MainWindow.xaml.cs
@@ -24,26 +24,39 @@ namespace GraficadorSenales
         {
             InitializeComponent();
 
-            plnGrafica.Points.Add(new Point(0, 6));
-            plnGrafica.Points.Add(new Point(100, 50));
-            plnGrafica.Points.Add(new Point(150, 46));
-            plnGrafica.Points.Add(new Point(155, 70));
-            plnGrafica.Points.Add(new Point(300, 100));
-            plnGrafica.Points.Add(new Point(433, 68));
-            plnGrafica.Points.Add(new Point(533, 100));
-            plnGrafica.Points.Add(new Point(633, 50));
-            plnGrafica.Points.Add(new Point(763, 68));
-            plnGrafica.Points.Add(new Point(1003, 68));
+
 
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void txtTiempoFinal_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        private void txtTiempoInicial_TextChanged(object sender, TextChangedEventArgs e)
         {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double amplitud = double.Parse(txtAmplitud.Text);
+            double fase = double.Parse(txtFase.Text);
+            double frecuencia = double.Parse(txtFrecuencia.Text);
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text); 
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text); 
+            double frecuenciaMuestreo = double.Parse(txtMuestreo.Text);
+
+            SenalSenoidal senal = new SenalSenoidal(amplitud, fase, frecuencia);
+
+            double periodoMuestreo = 1 / frecuenciaMuestreo;
+            plnGrafica.Points.Clear();
+
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo) 
+            {
+                plnGrafica.Points.Add(new Point(i * scrContenedor.Width, (senal.evaluar(i) * ((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
+
+            }
 
         }
     }
