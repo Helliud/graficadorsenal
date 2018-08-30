69d59bdc67a08052ad1a3d92bfc49a7f89461ffe
Javier Eliud <42249118+Helliud@users.noreply.github.com>
2018-08-30 10:41:44 -0700

    Se agrego un graficador de rampas

--- GraficadorSenales/MainWindow.xaml.cs
+++ GraficadorSenales/MainWindow.xaml.cs
@@ -43,37 +43,78 @@ namespace GraficadorSenales
             double amplitud = double.Parse(txtAmplitud.Text);
             double fase = double.Parse(txtFase.Text);
             double frecuencia = double.Parse(txtFrecuencia.Text);
-            double tiempoInicial = double.Parse(txtTiempoInicial.Text); 
-            double tiempoFinal = double.Parse(txtTiempoFinal.Text); 
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
             double frecuenciaMuestreo = double.Parse(txtMuestreo.Text);
 
             SenalSenoidal senal = new SenalSenoidal(amplitud, fase, frecuencia);
 
+
             double periodoMuestreo = 1 / frecuenciaMuestreo;
             plnGrafica.Points.Clear();
 
-            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo) 
+
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
             {
                 double valorMuestral = senal.evaluar(i);
 
-                if(Math.Abs(valorMuestral) > senal.AmplitudMaxima)
+                if (Math.Abs(valorMuestral) > senal.AmplitudMaxima)
                 {
                     senal.AmplitudMaxima = Math.Abs(valorMuestral);
 
                 }
 
-                senal.Muestras.Add(new Muestra(i, valorMuestral)); 
+                senal.Muestras.Add(new Muestra(i, valorMuestral));
 
             }
 
             //Recorrer una coleccion o arreglo
             foreach(Muestra muestra in senal.Muestras)
             {
+                plnGrafica.Points.Add(new Point(muestra.X* scrContenedor.Width, (muestra.Y* ((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
+
+
+            }
+        }
+
+        /*-------------------------------------*/
+        private void btnGraficasRampa_click(object sender, RoutedEventArgs e)
+        {
+            double tiempoInicial = double.Parse(txtTiempoInicial.Text); //TODAS LAS SENALES OCUPAN ESTAS 3 VARIABLES
+            double tiempoFinal = double.Parse(txtTiempoFinal.Text);
+            double frecuenciaMuestreo = double.Parse(txtMuestreo.Text);
+
+            Rampa senalRampa = new Rampa();
+
+
+            double periodoMuestreo = 1 / frecuenciaMuestreo;
+            plnGrafica.Points.Clear();
+
+
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
+            {
+                double valorMuestral = senalRampa.evaluar(i);
+
+                if (Math.Abs(valorMuestral) > senalRampa.AmplitudMaxima)
+                {
+                    senalRampa.AmplitudMaxima = Math.Abs(valorMuestral);
+
+                }
+
+                senalRampa.Muestras.Add(new Muestra(i, valorMuestral));
+
+            }
+
+            //Recorrer una coleccion o arreglo
+            foreach (Muestra muestra in senalRampa.Muestras)
+            {
                 plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width, (muestra.Y * ((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
 
 
             }
-
         }
     }
 }
+
+   
+
---
+++ GraficadorSenales/Rampa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSenales
+{
+    class Rampa
+    {
+     
+        public List<Muestra> Muestras { get; set; }
+        public double AmplitudMaxima { get; set; }
+
+        public Rampa()
+        {
+            Muestras = new List<Muestra>();
+            AmplitudMaxima = 0.0;
+
+        }
+
+      
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+
+           if(tiempo >= 0)
+            {
+                resultado = tiempo;
+            }
+
+           else
+            {
+                resultado = 0;
+            }
+
+            return resultado;
+
+
+        }
+    }
+}
